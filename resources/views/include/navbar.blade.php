e8064b740333d2927befeadb5d3d745348c1115f
Kenneth-joseph <josephochieng812@gmail.com>
2020-03-05 08:57:06 +0300

    made changes in my navbar

--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -1,5 +1,6 @@
-navbar
+
 <ul class="navbar-nav" style="list-style: none;">
     <li><a href="/">Home</a></li>
-    <li><a href="/availability">Schedue</a></li>
+    <li><a href="/availability">Schedue</a></li>  
     <li><a href="/create">Create Content</a></li>
+  </ul>
