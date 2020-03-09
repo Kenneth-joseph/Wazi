e2ad5f5b7129da8849fc67110fbc3c243b378924
kebasoh <ongatikebasoba@gmail.com>
2020-03-09 13:38:00 +0300

    updated the master

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,47 +2,5 @@
 @section('content')
     <h3>{{$title}}</h3>
        <p>Therapist dashboard</p>
-       <div class="header">
-    <div class="logo">
-        <i class="fa fa-tachometer"></i>
-        <span>Brand</span>
-    </div>
-    <a href="#" class="nav-trigger"><span></span></a>
-</div>
-<div class="side-nav">
-    <div class="logo">
-        <i class="fa fa-tachometer"></i>
-        <span>Brand</span>
-    </div>
-    <nav>
-        <ul>
-            <li>
-                <a href="#">
-                    <span><i class="fa fa-user"></i></span>
-                    <span>THERAPIST</span>
-                </a>
-            </li>
-            <li>
-                <a href="#>
-                    <span><i class="fa fa-envelope"></i></span>
-                    <span>Availability</span>
-                </a>
-            </li>
-            <li class="active">
-                <a href="#">
-                    <span><i class="fa fa-bar-chart"></i></span>
-                    <span>Create Content</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <span><i class="fa fa-credit-card-alt"></i></span>
-                    <span>Create Assessments</span>
-                </a>
-            </li>
-        </ul>
-    </nav>
-</div>
-<div class="main-content">
-</div>
+     
 @endsection
